b09afac4b3e4184f24aa4905d0edfbd0f31efe3c
James Russel C. Bautista <jrussclay@gmail.com>
2022-11-12 19:34:50 +0800

    Update page_size and max_page_size value for DefaultPagination

--- tasks/pagination.py
+++ tasks/pagination.py
@@ -2,5 +2,6 @@ from rest_framework.pagination import PageNumberPagination
 
 
 class DefaultPagination(PageNumberPagination):
-    page_size = 1
+    page_size = 20
+    max_page_size = 20
     page_size_query_param = 'page_size'

7379ddbfa910466534591462e6759a0c2351387c
James Russel C. Bautista <jrussclay@gmail.com>
2022-11-13 23:07:57 +0800

    Add check if category is exist in the request

--- tasks/permissions.py
+++ tasks/permissions.py
@@ -6,6 +6,8 @@ class TaskCategoryOwnPermission(permissions.BasePermission):
     def has_permission(self, request, view):
         if view.action == 'create' or view.action == 'update' or view.action == 'partial_update':
             category = request.data.get('category')
+            if category is None:
+                return True
             ## check whether category is owned by user
             user_category_exists = Category.objects.filter(created_by=request.user, id=category).exists()
             return user_category_exists
